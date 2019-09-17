2a3290774bd72ef5f7f44e0a4379cc438ce32c41
dsong <39391978+songdavidb@users.noreply.github.com>
2019-09-17 10:02:55 -0400

    loop fix, Q1

--- song_d_pset1.R
+++ song_d_pset1.R
@@ -6,7 +6,11 @@ for (num in num_lst)
 {
   set.seed(02138)
   assign(paste('lamb', num, sep=''), rpois(10000, num))
-  hist(get(paste('lamb', num, sep='')), 25)
+  x = get(paste('lamb', num, sep=''))
+  hist(x, 25)
+  mu = mean(x)
+  v = var(x)
+  cat(x, 'mean:', mu, 'variance:', v)
 }
 
 #test2
